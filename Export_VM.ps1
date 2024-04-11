8f367ad92700847090ba7217e8e33dcae40acb05
ZavarNick <emelyanoffon@gmail.com>
2024-04-11 14:57:09 +0500

    Экспорт VM с помощью Powershell с удаление старых VM старше трех дней

---
+++ Export_VM.ps1
@@ -0,0 +1,19 @@
+﻿# Экспорт VM из Hyper-V
+Export-VM -Name <NameVM> -Path "D:\export_vm"
+
+# Получение текущей даты и переименование
+$Now = Get-Date -format yyyy-MM-dd_HH-mm
+Rename-Item -Path D:\export_vm\<NameVM> -NewName "<NameVM>_$Now"
+
+# Параметры
+$Path = "D:\export_vm"
+$Days = 3 # Количество дней
+
+# Вычисляем дату, которая будет использоваться для фильтрации
+$CutoffDate = (Get-Date).AddDays(-$Days)
+
+# Удаляем файлы старше указанного количества дней
+Get-ChildItem -Path $Path -Recurse -File | Where-Object { $_.LastWriteTime -lt $CutoffDate } | Remove-Item –Force -Verbose
+
+# Удаляем пустые каталоги
+Get-ChildItem -Path $Path -Recurse -Directory | Where-Object { $_.GetFileSystemInfos().Count -eq 0 -and $_.CreationTime -lt $CutoffDate } | Remove-Item -Force -Recurse -Verbose
